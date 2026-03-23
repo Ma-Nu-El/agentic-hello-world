7535327562a7f90e8a8f4d0dcc2e99d39299888f
Manuel Fuica Morales <m.fuica01@ufromail.cl>
2026-03-23 23:35:22 +0000

    Add feature test for hello route

---
+++ tests/Feature/HelloRouteTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class HelloRouteTest extends TestCase
+{
+    public function test_hello_route_returns_hello_world(): void
+    {
+        $response = $this->get('/hello');
+
+        $response->assertStatus(200);
+        $response->assertSeeText('Hello World');
+    }
+}
